2bb78dd281abaeaa52ac7639f93c8b4e3ffdb656
Ran Mishael <ran@lavanet.xyz>
2023-10-22 14:40:01 +0200

    add home icons

--- components/navbarTxPage.js
+++ components/navbarTxPage.js
@@ -3,10 +3,15 @@ import ThemeChanger from "./DarkSwitch";
 import Image from "next/image"
 import { Disclosure } from "@headlessui/react";
 import AccountInfo from "./accountInfo";
+import { useTheme } from "next-themes"; // Import the useTheme hook
+
 const Navbar = () => {
   const navigation = [
     // "Home"
   ];
+
+  const { theme } = useTheme(); // Get the current theme
+  const homeImageSrc = theme === "dark" ? "/img/home2.png" : "/img/homeBlack.png";
 
   return (
     <div className="w-full">
@@ -20,14 +25,14 @@ const Navbar = () => {
                   <span className="flex items-center space-x-2 text-2xl font-medium text-indigo-500 dark:text-gray-100">
                     <span>
                       <Image
-                        src="/img/logo.png"
+                        src={homeImageSrc}
                         alt="N"
                         width="32"
                         height="32"
                         className="w-8"
                       />
                     </span>
-                    <span>Return</span>
+                    <span></span>
                   </span>
                 </Link>
                 <Disclosure.Button
