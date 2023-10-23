c23cb2c739afe3eb67e3db087cfe2ae1a45b1200
Ran Mishael <ran@lavanet.xyz>
2023-10-23 17:20:56 +0200

    fix print

--- components/hero.js
+++ components/hero.js
@@ -14,7 +14,7 @@ const Hero = () => {
               Lava - Evmos iPRPC Reward Distribution
             </h1>
             <p className="py-5 text-xl leading-normal text-gray-500 lg:text-xl xl:text-2xl dark:text-gray-300">
-              Connect your wallet to the smart contract and distribute the iPRPC rewards to a list of Evmos Providers
+            Connect your wallet to distribute iPRPC rewards to Evmos Providers
             </p>
 
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
