f36bceb2a1b23858168fc97e98e105f3e6f09128
Gil <gil@lavanet.xyz>
2023-11-08 12:18:32 -0500

    update testnet contract

--- components/utils.js
+++ components/utils.js
@@ -1,3 +1,3 @@
-const EvmosTestnetContract = "0x02bBCa20734bEc605A9BBB87123253EB1975D1E1"
+const EvmosTestnetContract = "0xca0b2d58F938a7bf7a49AC167b62D5e484b5bfd8"
 const EvmosMainnetContract = ""
 export const ContractAddress = EvmosTestnetContract; // Replace with your contract address
