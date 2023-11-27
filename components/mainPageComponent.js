f0fa1fc231c53ab50a3ef88e208ba22826edf345
Ran Mishael <ran@lavanet.xyz>
2023-11-27 16:52:36 +0100

    fixing the github href

--- components/mainPageComponent.js
+++ components/mainPageComponent.js
@@ -20,7 +20,7 @@ const MainPageComponent = () => {
             <div className="flex flex-col items-start space-y-3 sm:space-x-4 sm:space-y-0 sm:items-center sm:flex-row">
               <MetaMaskConnectButton />
               <a
-                href="https://github.com/lavanet/evmos-iprpc-payment-contract"
+                href="https://github.com/lavanet/lava-iprpc-payment-contract"
                 target="_blank"
                 rel="noopener"
                 className="flex items-center space-x-2 text-gray-500 dark:text-gray-400">
